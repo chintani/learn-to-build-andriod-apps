506e8ca29e5659886b5eb49670256b732568fa16
Tania Chin <cybercorpindustries@gmail.com>
2014-04-16 17:35:42 -0400

    Added more info to MainActivity.java

--- first_app/MyFirstApp/src/com/chintani/myfirstapp/MainActivity.java
+++ first_app/MyFirstApp/src/com/chintani/myfirstapp/MainActivity.java
@@ -66,6 +66,9 @@ public class MainActivity extends ActionBarActivity {
 /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
 		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
         // Do something in response to button
     }
 
