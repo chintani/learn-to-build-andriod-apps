eafb8026863e0b1a5ac544b0a653f3ecd4e57d36
Tania Chin <cybercorpindustries@gmail.com>
2014-06-25 12:46:26 -0400

    Crystal Ball MainActivity.java update
    Update made by me

--- crystall_ball/CrystalBall/src/com/example/crystalball/MainActivity.java
+++ crystall_ball/CrystalBall/src/com/example/crystalball/MainActivity.java
@@ -1,15 +1,11 @@
 package com.example.crystalball;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -17,6 +13,12 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // Declare our View variables and assign them the Views from the layout file
+        TextView answerLabel = (TextView) findViewById(R.id.textView1);
+        Button getAnswerButton = (Button) findViewById(R.id.button1);
+        
+        
     }
 
 
