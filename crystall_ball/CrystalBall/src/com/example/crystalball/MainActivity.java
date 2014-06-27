c16f4739c6327631a70d29fcf22ea6ac485e9d96
Tania Chin <cybercorpindustries@gmail.com>
2014-06-27 01:28:33 -0400

    Crystal Ball MainActivity.java update
    Update made by me.

--- crystall_ball/CrystalBall/src/com/example/crystalball/MainActivity.java
+++ crystall_ball/CrystalBall/src/com/example/crystalball/MainActivity.java
@@ -16,14 +16,16 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         
         // Declare our View variables and assign them the Views from the layout file
-        TextView answerLabel = (TextView) findViewById(R.id.textView1);
+        final TextView answerLabel = (TextView) findViewById(R.id.textView1);
         Button getAnswerButton = (Button) findViewById(R.id.button1);
         
         getAnswerButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
+				// The button was clicked, so update the answer label with an answer
+				String answer = "Yes";
+				answerLabel.setText(answer);
 				
 			}
 		});
