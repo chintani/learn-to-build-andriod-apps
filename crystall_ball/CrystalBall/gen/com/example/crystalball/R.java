348f5a400d0cf6ad726232ff4625d718f2736271
Tania Chin <cybercorpindustries@gmail.com>
2014-07-28 19:11:59 -0400

    Default eclipse crystal ball files update

--- crystall_ball/CrystalBall/gen/com/example/crystalball/R.java
+++ crystall_ball/CrystalBall/gen/com/example/crystalball/R.java
@@ -1016,7 +1016,7 @@ containing a value of this type.
         public static final int icon=0x7f05002a;
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
-        public static final int imageView1=0x7f05003c;
+        public static final int imageView1=0x7f05003d;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
@@ -1043,7 +1043,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int textView1=0x7f05003d;
+        public static final int textView1=0x7f05003c;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
