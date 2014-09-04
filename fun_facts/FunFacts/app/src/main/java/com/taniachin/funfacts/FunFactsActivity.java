5ae000e961ac7f4e5615d5b4c1661cf24441f131
Tania Chin <cybercorpindustries@gmail.com>
2014-09-04 12:21:34 -0400

    Added a "listener" that listens for the button being pressed.

--- fun_facts/FunFacts/app/src/main/java/com/taniachin/funfacts/FunFactsActivity.java
+++ fun_facts/FunFacts/app/src/main/java/com/taniachin/funfacts/FunFactsActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
-//import android.view.View;
+import android.view.View;
 
 
 public class FunFactsActivity extends Activity {
@@ -25,11 +25,11 @@ public class FunFactsActivity extends Activity {
             public void onClick(View view) {
 
             }
-        }
+        };
 
 
 
-        showFactButton.setOnClickListener();
+        showFactButton.setOnClickListener(listener);
     }
 
 
