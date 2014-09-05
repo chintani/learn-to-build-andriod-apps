db62a4c857eb3df45389ffadd4a79dfc66b2d6e3
Tania Chin <cybercorpindustries@gmail.com>
2014-09-05 01:35:13 -0400

    Created a new class to handle just the part of the code relating to getting the facts.

---
+++ fun_facts/FunFacts/app/src/main/java/com/taniachin/funfacts/FactBook.java
@@ -0,0 +1,36 @@
+package com.taniachin.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by Admin on 9/5/2014.
+ */
+public class FactBook {
+
+    // Member variable (properties about the object)
+
+    // Method (abilities: things the object can do)
+    public String getFact(){
+
+        String [] facts = {
+                "Ants stretch when they wake up in the morning.",
+                "Ostriches can run faster than horses.",
+                "Olympic gold medals are actually made mostly of silver",
+                "You are born with 300 bones; by the time you are an adult you will have 206.",
+                "It takes about 8 minutes for light from the sun to reach Earth.",
+                "Some bamboo plants can grow almost a meter in just one day.",
+                "The state of Florida is bigger than England.",
+                "Some penguins can leap 2-3 meters out of the water",
+                "On average, it takes 66 days to form a new habit.",
+                "Mammoths still walked the Earth when the Great Pyramid was being built."
+        };
+        // The button was clicked, so update the fact label with a new fact
+        String fact = "";
+        //Randomly select a fact
+        Random randomGenerator = new Random(); //Construct a new Random number generator
+        int randomNumber = randomGenerator.nextInt(facts.length);
+
+        fact = facts[randomNumber];
+        return fact;
+    }
+}
