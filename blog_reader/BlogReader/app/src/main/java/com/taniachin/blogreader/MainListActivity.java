3a5c6473e7be407d7df530cc1ebf5d3cd48393c5
Tania Chin <cybercorpindustries@gmail.com>
2014-10-08 01:26:01 -0400

    Added a for loop to loop through all the blog post, get the title for each one, converted the special characters etc.

--- blog_reader/BlogReader/app/src/main/java/com/taniachin/blogreader/MainListActivity.java
+++ blog_reader/BlogReader/app/src/main/java/com/taniachin/blogreader/MainListActivity.java
@@ -6,8 +6,10 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -65,9 +67,15 @@ public class MainListActivity extends ListActivity {
                 for (int i = 0; i < jsonPosts.length(); i++){
                     JSONObject post = jsonPosts.getJSONObject(i);
                     String title = post.getString("title");
+                    title = Html.fromHtml(title).toString();
                     mBlogPostTitles[i] = title;
+
                 }
-            } catch (JSONException e) {
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1, mBlogPostTitles);
+                setListAdapter(adapter);
+            }
+            catch (JSONException e) {
                 Log.e(TAG, "Exception caught", e);
             }
         }
