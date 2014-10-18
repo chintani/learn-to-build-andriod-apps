b9b0cb2027adc68b28050a4c357fd1f21fc7d93d
Tania Chin <cybercorpindustries@gmail.com>
2014-10-18 19:23:14 -0400

    Created an array of items to be displayed in app. Created adapter and defined how the information should be laid out.

---
+++ red_shoes_r_us/RedShoesRUs/app/src/main/java/com/taniachin/redshoesrus/HomeActivity.java
@@ -0,0 +1,56 @@
+package com.taniachin.redshoesrus;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+
+public class HomeActivity extends ListActivity {
+
+    protected String [] mAndroidNames = {
+            "Android beta",
+            "Android 1.0",
+            "Android 1.1",
+            "Cupcake",
+            "Donut",
+            "Eclair",
+            "Froyo",
+            "Gingerbread",
+            "Honeycomb",
+            "Ice cream Sandwich",
+            "Jelly Bean"
+
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1 ,mAndroidNames);
+        setListAdapter(adapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
